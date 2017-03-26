aaa3dc45de86d990f2bedba999d44367d70aedaa
ursula <ulma83@poczta.fm>
2017-03-26 10:44:04 +0200

    add comments and alerts

---
+++ script.js
@@ -0,0 +1,15 @@
+//plik.script
+//alert('test')
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, Ula');
+console.log('Witaj, Ula');
+
+//console.log(c); - error no define c
+
+/*kliknąć przycisk aby wyświetlić alert box 
+z tekstem "Hello\How are you"*/
+function myFunction() {
+    //alert("Hello\nHow are you?"); together
+    alert("Hello!"); //separately
+    alert("How are you?"); //separately
+}
