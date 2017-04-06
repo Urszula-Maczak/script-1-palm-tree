b50863b14c1b6fff1bfa87711e3a88e3b8a4ab2b
ursula <ulma83@poczta.fm>
2017-04-07 00:10:26 +0200

    add value zero and bonus

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,22 @@
 //script.js
 
-var a = 3;
-var b = 4;
+var a = 0;
+var b = 0;
 var value = (a * a) + (2 * a * b) - (b * b);
-console.log('Wynik to' + value);
-if (value > '0') {
-	console.log('Wartość jest dodatnia');
-} else if (value < '0') {
-	console.log('Wartość jest ujemna');
-}
-// a czy może być tez tak:
+console.log('Wynik to ' + value);
 
-var value = (a * a) + (2 * a * b) - (b * b) > 0 ? 'Wartość (a * a) + (2 * a * b) - (b * b) jest dodatnia' : 'Wartość (a * a) + (2 * a * b) - (b * b) jest ujemna';
-console.log(value);
+if (value > 0) {
+	console.log('Wartość jest dodatnia');
+} else if (value < 0) {
+	console.log('Wartość jest ujemna');
+} else {
+	console.log('Wartość zerowa');
+}
+
+if (value == 0) {
+	console.log('Wartość zerowa');
+} else if (value != 0) {
+	var value = (a * a) + (2 * a * b) - (b * b) > 0 ? 'Wartość (a * a) + (2 * a * b) - (b * b) jest dodatnia' : 'Wartość (a * a) + (2 * a * b) - (b * b) jest ujemna';
+	console.log(value);
+}
+
