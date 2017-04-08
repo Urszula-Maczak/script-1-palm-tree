5af777d5816377a4d0bb42b97206a3ab5e8cc7a1
ursula <ulma83@poczta.fm>
2017-04-08 17:57:20 +0200

    remove dot

--- javascript/script.js
+++ javascript/script.js
@@ -6,9 +6,16 @@ var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
 
 var	newName = "Marian";
-if (allNames.indexOf(newName) === -1.) {
+if (allNames.indexOf(newName) === -1) {
 	console.log('Wyrażenie jest spełnione');
 }
 
 allNames.push('Marian');
 console.log(allNames);
+
+var names = ['Asia', 'Kasia', 'Ola', 'Sylwia', 'Ola'];
+var indexOfOla = names.indexOf('Ola');
+
+console.log('szukana wartość znajduje się na ' + indexOfOla + ' pozycji tablicy');
+
+console.log(names[indexOfOla]);
