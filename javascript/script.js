d7347bcd11348f9a338aaf1baae7d39f317bead2
ursula <ulma83@poczta.fm>
2017-04-08 14:52:25 +0200

    remove if

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 //script.js
 
-var a = 0;
-var b = 0;
+var a = -2;
+var b = -8;
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log('Wynik to ' + value);
 
@@ -15,7 +15,7 @@ if (value > 0) {
 
 if (value == 0) {
 	console.log('Wartość zerowa');
-} else if (value != 0) {
+} else /*if -niepotrzebny*/ (value != 0) {
 	var value = (a * a) + (2 * a * b) - (b * b) > 0 ? 'Wartość (a * a) + (2 * a * b) - (b * b) jest dodatnia' : 'Wartość (a * a) + (2 * a * b) - (b * b) jest ujemna';
 	console.log(value);
 }
