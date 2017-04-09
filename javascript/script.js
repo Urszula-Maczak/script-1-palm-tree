856a252796b84a1677076d99f4ce610b87eee2f4
ursula <ulma83@poczta.fm>
2017-04-10 01:27:39 +0200

    delete kod and it is empty file

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,1 @@
-//ŁĄCZYMY TABLICE
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
 
-console.log(allNames);
-
-var	newName = "Marian"
-if (allNames.indexOf(newName) === -1.) {
-	console.log('Wyrażenie jest spełnione');
-} else {
-	console.log(allNames[newName])
-}
-
-var x = allNames.push('Marian');
-console.log(allNames);
