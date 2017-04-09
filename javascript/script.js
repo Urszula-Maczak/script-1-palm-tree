581038e8a92d640d1ea642c9ef75618090b89808
ursula <ulma83@poczta.fm>
2017-04-09 08:10:48 +0200

    niepotrzebna zmiana, nie zapisana

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
 
 //Papugi i słonie
 
-var text = "Krowy koszą trawę, unosząćs się nad ziemią 15 centymetrów, na czerownych dywanach";
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerownych dywanach";
 var animal = "Zielone słonie";
 var animalUpperCased = animal.toUpperCase();
 
@@ -13,6 +13,6 @@ var textChangedForAnimal = text.replace(chooseFirstWord, animal);
 
 console.log(textChangedForAnimal);
 
-var partOfText = textChangedForAnimal.slice(0,46);
+var partOfText = textChangedForAnimal.slice('<br/>');
 
 console.log(partOfText);
