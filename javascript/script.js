21365d1e585e6a7ed8549737ab5c062525374f18
ursula <ulma83@poczta.fm>
2017-04-10 08:48:31 +0200

    creat xmas tree and I'm happy

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,21 @@
+//choinka
+//	var level = 5; 
+function drawTree(level) {
+	for (var i = 1; i <= level; i++) {
+		
+		var star = '';
+		for (var j = 0; j < i*2-1; j++) {
+			star += '*'; // star = star + '*' albo star++
+		}
+		//console.log(star); - nierówna choinka
+		console.log(' '.repeat(level - i) + '*'.repeat(i*2-1));	//wyśrodkowana choinka	
+	}
+}
+drawTree(5);
+
+/*Notatki:
+level - ilość poziomów (parametr funkcji albo zmienna)
+i - liczba aktualnego poziomu
+j - liczy ilość iteracji (powtarzanie się pętli)
+i*2-1 - ciąg, który liczy ilość gwiazdek dla danego poziomu */
 
