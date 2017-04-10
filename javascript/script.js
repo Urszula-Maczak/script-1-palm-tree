77b4d014b2d3c6a4861924004827c6716d3c5317
ursula <ulma83@poczta.fm>
2017-04-10 23:13:34 +0200

    YES, I did it. I found the way for half the text.

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
 
 //Papugi i słonie
 
-var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerownych dywanach";
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerownych dywanach.";
 var animal = "Zielone słonie";
 var animalUpperCased = animal.toUpperCase();
 
@@ -13,6 +13,6 @@ var textChangedForAnimal = text.replace(chooseFirstWord, animal);
 
 console.log(textChangedForAnimal);
 
-var partOfText = textChangedForAnimal.slice('<br/>');
+var partOfText = textChangedForAnimal.slice(0, textChangedForAnimal.length/2);
 
 console.log(partOfText);
