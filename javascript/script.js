366603f22d4870ee45a3a87ab5d197b90e99e95d
ursula <ulma83@poczta.fm>
2017-04-10 22:02:30 +0200

    remove all condition if

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 //script.js
 
-var a = -2;
-var b = -8;
+var a = 0;
+var b = 0;
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log('Wynik to ' + value);
 
@@ -13,10 +13,10 @@ if (value > 0) {
 	console.log('Wartość zerowa');
 }
 
-if (value == 0) {
+/*if (value == 0) {
 	console.log('Wartość zerowa');
-} else /*if -niepotrzebny*/ (value != 0) {
+} else if (value != 0) {
 	var value = (a * a) + (2 * a * b) - (b * b) > 0 ? 'Wartość (a * a) + (2 * a * b) - (b * b) jest dodatnia' : 'Wartość (a * a) + (2 * a * b) - (b * b) jest ujemna';
-	console.log(value);
-}
+	console.log('Wartość zerowa'); - niepotrzebny warunek if
+}*/
 
